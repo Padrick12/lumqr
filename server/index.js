7a6137b8ac7652a829c5020c211a91575f1311a4
Padrick12 <adricklol400@gmail.com>
2026-08-26 19:01:04 -0600

    Add ngrok-skip-browser-warning header middleware to server

--- server/index.js
+++ server/index.js
@@ -9,6 +9,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader('ngrok-skip-browser-warning', 'true');
+  next();
+});
+
 // Serve static frontend assets if built
 const clientDistPath = path.join(__dirname, '../client/dist');
 app.use(express.static(clientDistPath));
